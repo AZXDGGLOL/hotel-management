4923d63fa43bc6e4560e34b764eef22b0439fca9
Anuntasit Singcha <archizombie12345@gmail.com>
2026-03-13 15:20:25 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace hotel_management
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ room type.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hotel_management
+{
+    public partial class room_type : Form
+    {
+        public room_type()
+        {
+            InitializeComponent();
+        }
+    }
+}
